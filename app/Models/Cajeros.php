728fc3bd9a88600c263759ce4c04a4abb25a6c87
DanielVargasHdz <danixasce17@gmail.com>
2024-07-04 17:02:46 -0600

    Cajeros Api

---
+++ app/Models/Cajeros.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cajeros extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'numero',
+        'nombre',
+        'direccion',
+        'colonia',
+        'estado',
+        'telefono',
+        'fax',
+        'mail',
+        'baja',
+        'fec_cambio',
+        'clave_cajero',
+    ];
+    protected $attributes = [
+        'numero'=>'0',
+        'nombre'=>'',
+        'direccion'=>'',
+        'colonia'=>'',
+        'estado'=>'',
+        'telefono'=>'',
+        'fax'=>'',
+        'mail'=>'',
+        'baja'=>'',
+        'fec_cambio'=>'',
+        'clave_cajero'=>'0',
+    ];
+    protected $table ='cajeros';
+    protected $primaryKey = 'numero';
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TipoCobroController;
+use App\Http\Controllers\CajeroController;
 
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,4 +15,13 @@ Route::middleware('auth:sanctum')->controller(TipoCobroController::class)->group
     Route::get("/tipo_cobro/siguiente", "siguiente");
     Route::post('/tipo_cobro', 'store');
     Route::post('/tipo_cobro/update', 'update');
+});
+
+//Cajeros
+Route::middleware('auth:sanctum')->controller(CajeroController::class)->group(function () {
+    Route::post('/Cajero','PostCajeros');
+    Route::post('/Cajero/UpdateCajeros','UpdateCajeros');
+    Route::get('/Cajero/baja','indexBaja');
+    Route::get("/Cajero","index");
+    Route::get("/Cajero/siguiente", "siguiente");
 });
