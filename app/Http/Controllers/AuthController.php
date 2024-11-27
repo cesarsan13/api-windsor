9dc3e8badd8e0213c695f87de1bf31bc1a1e6f33
Abel <lujanlom0120@gmail.com>
2024-11-27 11:24:56 -0600

    cambios en el objeto de usuario, se le agrego el arreglo de permisos

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Acceso_Usuario;
 use Illuminate\Http\Request;
 use App\Models\ObjectResponse;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -21,13 +23,16 @@ class AuthController extends Controller
             'password.required' => 'El campo "Contraseña" es obligatorio',
         ]);
 
-
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()->all()], 422);
         }
         $user = User::where('email', $request->email)
             ->where('baja', "<>", "*")
             ->first();
+        $access = Acceso_Usuario::where('id_usuario', '=', $user->id)->get();
+        // Log::info($access);
+        $user->permissions = $access;
+        // Log::info($user);
         if (!$user || !Hash::check($request->password, $user->password)) {
             $response = ObjectResponse::CatchResponse("Credenciales incorrectas");
             return response()->json($response, 404);
@@ -39,5 +44,4 @@ class AuthController extends Controller
         data_set($response, 'data', $user);
         return response()->json($response, $response['status_code']);
     }
-
 }
