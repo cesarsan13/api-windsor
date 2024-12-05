50e2378a8d6d9aec4fb318ea3892659591624a03
AndreaMejia3 <andreamejia3333@outlook.com>
2024-12-05 13:45:34 -0600

    agregue registros a  AccesoUsuarioSeeder para darles permisos al usuario 1 el cual es admin 01:45pm 05/12/2024

--- database/seeders/AccesoUsuarioSeeder.php
+++ database/seeders/AccesoUsuarioSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AccesoUsuarioSeeder extends Seeder
 {
@@ -12,6 +13,417 @@ class AccesoUsuarioSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $registros = [
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 1,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 2,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 3,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 4,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 5,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 6,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 7,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 8,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 9,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 10,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 11,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 12,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 13,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 14,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 15,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 16,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 17,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 18,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 19,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 20,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 21,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 22,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 23,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 24,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 25,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 26,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 27,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 28,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 29,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 30,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 31,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 32,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 33,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 34,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 35,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 36,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 37,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 38,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],
+            [
+                'id_usuario' => 1,
+                'id_punto_menu' => 39,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 40,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 41,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 42,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 43,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 44,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ],[
+                'id_usuario' => 1,
+                'id_punto_menu' => 45,
+                't_a' => 1,
+                'altas' => 1,
+                'bajas' => 1,
+                'cambios' => 1,
+                'impresion' => 1 
+            ]
+        ];
+
+        foreach($registros as $registro){
+            DB::table('acceso_usuario')->updateOrInsert(
+                [
+                    'id_usuario' => $registro['id_usuario'],
+                    'id_punto_menu' => $registro['id_punto_menu'],
+                    't_a' => $registro['t_a'],
+                    'altas' => $registro['altas'],
+                    'bajas' => $registro['bajas'],
+                    'cambios' => $registro['cambios'],
+                    'impresion' => $registro['impresion']],
+                    $registro
+            );
+        }
+
     }
 }
