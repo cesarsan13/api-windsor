1164fad19b6fed44a2b19a2ea8385a07b43830ae
DanielVargasHdz <danixasce17@gmail.com>
2025-03-06 13:39:19 -0600

    implementacion de codigo de verificacion

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
-use Illuminate\Support\Str;
-use App\Http\Controllers\MailController;
 
 class RegisterController extends Controller
 {
@@ -17,15 +15,13 @@ class RegisterController extends Controller
         'required' => 'El campo :attribute es obligatorio.',
         'max' => 'El campo :attribute no puede tener más de :max caracteres.',
         'min' => 'El campo :attribute debe tener al menos :min caracteres.',
-        'unique' => 'El  :attribute ya ha sido registrado anteriormente.',
+        'unique' => 'El :attribute ya ha sido registrado anteriormente.',
     ];
     protected $rules = [
         'name' => 'required|string|max:250|unique:users',
         'nombre' => 'required|string|max:50|unique:users',
         'email' => 'required|email|max:40|unique:users',
-        //'password' => 'required|string|min:6',
-        //'numero_prop' => 'required|integer',
-        //'baja' => 'nullable|string|max:1',
+        'password' => 'required|string|min:6',
     ];
 
     public function Register(Request $request)
@@ -46,37 +42,22 @@ class RegisterController extends Controller
                 'name',
                 'email',
                 'nombre',
-                'numero_prop',
-                'baja'
+                'password'
             ]);
-
-            $passwordgenerate = Str::random(10);
 
             $nuevoUsuario = User::create([
                 "id" => $usuario,
                 "name" => $datosFiltradosUsuario['name'],
                 "nombre" => $datosFiltradosUsuario['nombre'],
                 "email" => $datosFiltradosUsuario['email'],
-                "password" => Hash::make($passwordgenerate),
+                "password" => Hash::make($datosFiltradosUsuario['password']),
                 "numero_prop" => 1,
                 "baja" => '',
             ]);
 
-            $DataI = [
-                "title" => "Bienvenido/a {$datosFiltradosUsuario['nombre']}",
-                "title2" => "Generación de Contraseña",
-                "body" => "Estimado/a {$datosFiltradosUsuario['nombre']}, su contraseña para el inicio de sesion es la siguiente: {$passwordgenerate}",
-                "view" => "mail-template",
-                "email" => $datosFiltradosUsuario['email'],
-            ];
-
-            $mailController = new MailController();
-            $resultado = $mailController->enviarNuevaContrasenaRegister($DataI);
-
             $response = ObjectResponse::CorrectResponse();
-            data_set($response, 'message', 'peticion satisfactoria | Usuario registrado.');
-            data_set($resultado, 'messageMail', 'Peticion satisfactoria | Contraseña Enviada');
-            data_set($response, 'alert_text', 'Usuario registrado exitosamente.<br>Se ha enviado un correo electrónico con su contraseña para acceder al sistema.');
+            data_set($response, 'message', 'Petición satisfactoria | Usuario registrado.');
+            data_set($response, 'alert_text', 'Usuario registrado exitosamente.');
         } catch (\Exception $e) {
             $response = ObjectResponse::CatchResponse($e->getMessage());
         }
