c142ae5d6a38af59abd9e65b90157cbf13fcc40e
AndreaMejia3 <andreamejia3333@outlook.com>
2025-03-18 17:13:42 -0600

    Actualizacion en Act Cobranza 05:11pm 18/03/2025

--- app/Http/Controllers/ActCobranzaController.php
+++ app/Http/Controllers/ActCobranzaController.php
@@ -16,6 +16,7 @@ class ActCobranzaController extends Controller
         'max' => 'El campo :attribute no puede tener más de :max caracteres.',
         'unique' => 'El  :attribute ya ha sido registrado anteriormente',
     ];
+    
     protected $rules =[
         'alumno'=>'required|integer',
         'producto'=>'required|string',
@@ -24,10 +25,10 @@ class ActCobranzaController extends Controller
         'importe'=>'required|numeric',
         'descuento'=>'required|numeric'
     ];
+
     public function getDocumentosAlumno(Request $request) {
         $response = ObjectResponse::DefaultResponse();
         $documentoAlumno = DB::table('documentos_cobranza')
-        //->join('productos', 'documentos_cobranza.producto', '=', 'productos.numero')
         ->join('productos', DB::raw('CAST(documentos_cobranza.producto AS CHAR)'), '=', DB::raw('CAST(productos.numero AS CHAR)'))
         ->select('documentos_cobranza.producto','productos.descripcion','documentos_cobranza.numero_doc','documentos_cobranza.fecha','documentos_cobranza.importe','documentos_cobranza.descuento','documentos_cobranza.fecha_cobro','documentos_cobranza.importe_pago')
         ->where('alumno','=',$request->alumno)->get();
@@ -36,6 +37,7 @@ class ActCobranzaController extends Controller
         data_set($response, 'data', $documentoAlumno);
         return response()->json($response, $response['status_code']);
     }
+
     public function postActCobranza(Request $request) {
         $validator = Validator::make($request->all(), $this->rules, $this->messages);
         $response = ObjectResponse::DefaultResponse();
@@ -49,7 +51,6 @@ class ActCobranzaController extends Controller
             $alert_text = "El registro ya ha sido registrado anteriormente.";
             $response = ObjectResponse::BadResponse($alert_text);
             data_set($response, 'message', 'Informacion no valida');
-            // data_set($response, 'data', "El registro ya ha sido registrado anteriormente");
             return response()->json($response, $response['status_code']);
         }
         if ($validator->fails()) {
@@ -79,31 +80,39 @@ class ActCobranzaController extends Controller
         data_set($response, 'message', 'Petición satisfactoria : Datos insertados correctamente');
         return response()->json($response, $response['status_code']);
     }
+
     public function updateActCobranza(Request $request) {
+        $response = ObjectResponse::CorrectResponse();
         $validator = Validator::make($request->all(), $this->rules, $this->messages);
-        $response = ObjectResponse::DefaultResponse();
         if ($validator->fails()) {
-            Log::info('no valido');
             $alert_text = implode("<br>", $validator->messages()->all());
             $response = ObjectResponse::BadResponse($alert_text);
-            data_set($response, 'errors', $validator->errors());
+            data_set($response, 'message', 'Información no válida.');
             data_set($response, 'alert_icon', 'error');
             return response()->json($response, $response['status_code']);
         }
-        $actCobranza = DocsCobranza::where('alumno',$request->alumno)
-        ->where('producto',$request->producto)
-        ->where('numero_doc',$request->numero_doc)
-        ->where('fecha',$request->fecha)
-        ->update([
-            'fecha'=>$request->fecha,
-            'importe'=>$request->importe,
-            'descuento'=>$request->descuento
-        ]);
-        $response = ObjectResponse::CorrectResponse();
-        data_set($response, 'message', 'Peticion satisfactoria : Datos Actualizados');
-        data_set($response, 'data', $actCobranza);
-        return response()->json($response, $response['status_code']);
+        try {
+            DocsCobranza::where('alumno', $request->alumno)
+            ->where('producto', $request->producto)
+            ->where('numero_doc', $request->numero_doc)
+            ->where('fecha', $request->fecha)
+            ->update([
+                'fecha' => $request->fecha,
+                'importe' => $request->importe,
+                'descuento' => $request->descuento
+            ]);
+            $response = ObjectResponse::CorrectResponse();
+            data_set($response, 'message', 'Peticion satisfactoria : Datos Actualizados');
+            data_set($response, 'alert_text', 'Datos Actualizados');
+            return response()->json($response, $response['status_code']);
+
+        } catch (\Exception $ex) {
+            $response = ObjectResponse::CatchResponse($ex->getMessage());
+            data_set($response, 'alert_text', 'Error en actualizar los datos');
+            return response()->json($response, $response['status_code']);
+        }
     }
+
     public function deleteActCobranza(Request $request) {
         $response = ObjectResponse::DefaultResponse();
         $actCobranza = DocsCobranza::where('alumno',$request->alumno)
